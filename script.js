0545307b51fa39e6fd93a723549b19d650a2d49e
orlandogordon <ogordon1998@gmail.com>
2022-05-15 23:10:56 -0400

    code war challenge may 15th

---
+++ script.js
@@ -0,0 +1,43 @@
+// let x = [];
+
+// let a = "1 3 4 5 2 6 7 8 43";
+
+// a = a
+//   .split(" ")
+//   .map((el) => +el)
+//   .sort((a, b) => a - b);
+// console.log(`${a.shift()}`);
+// console.log(a.pop());
+
+// console.log(a);
+
+// let z = undefined;
+// if (z) {
+//   console.log("did not work");
+// } else {
+//   console.log("worked");
+// }
+
+// z = "1";
+// k = z.concat(" ", z);
+// console.log(k);
+
+// function highAndLow(numbers) {
+//   // ...
+//   numbers = numbers
+//     .split(" ")
+//     .map((el) => +el)
+//     .sort((first, second) => first - second);
+//   const smallest = `${numbers.shift()}`;
+//   const largest = `${numbers.pop()}`;
+//   // const largest = undefined;
+//   let result = "";
+//   if (largest == "undefined") {
+//     result = `${smallest} ${smallest}`;
+//   } else {
+//     result = largest.concat(" ", smallest);
+//   }
+//   return result;
+// }
+
+// console.log(highAndLow("42"));
