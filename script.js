c330f8124ac62b2e17e2d1ada756a5142749edf5
orlandogordon <ogordon1998@gmail.com>
2022-07-25 22:26:39 -0400

    Daily Code Wars Challenge Practicing Linked Lists

--- script.js
+++ script.js
@@ -52,20 +52,164 @@ function getPINs(observed) {
 // console.log(getPINs('369'), ["339", "366", "399", "658", "636", "258", "268", "669", "668", "266", "369", "398", "256", "296", "259", "368", "638", "396", "238", "356", "659", "639", "666", "359", "336", "299", "338", "696", "269", "358", "656", "698", "699", "298", "236", "239"])
 
 ////////////////////////////////////////
-// ------------  July 24th   ------------
-function findShort(s) {
-  return s
-    .split(" ")
-    .map((el) => el.length)
-    .sort((a, b) => a - b)[0];
+// ------------  July 25th   ------------
+// Linked List Practice
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
 }
 
-console.log(
-  findShort(
-    "turns out random test cases are easier than writing out basic ones"
-  ),
-  3
-);
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  // Inset first node
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+    this.size++;
+  }
+
+  // Insert last node
+  insertLast(data) {
+    let node = new Node(data);
+    let current;
+
+    // If empty, make head
+    if (!this.head) {
+      this.head = node;
+    } else {
+      current = this.head;
+
+      while (current.next) {
+        current = current.next;
+      }
+
+      current.next = node;
+    }
+
+    this.size++;
+  }
+
+  // Insert at index
+  insertAt(data, index) {
+    // If index is out of range
+    if (index > 0 && index > this.size) {
+      return;
+    }
+
+    // If first index
+    if (index === 0) {
+      this.insertFirst(data);
+      return;
+    }
+
+    const node = new Node(data);
+    let current, previous;
+
+    // Set current to first
+    current = this.head;
+    let count = 0;
+
+    while (count < index) {
+      previous = current; // Node before index
+      count++;
+      current = current.next; // Node after index
+    }
+
+    node.next = current;
+    previous.next = node;
+
+    this.size++;
+  }
+
+  // Get at index
+  getAt(index) {
+    let current = this.head;
+    let count = 0;
+
+    while (current) {
+      if (count == index) {
+        console.log(current.data);
+      }
+      count++;
+      current = current.next;
+    }
+    return null;
+  }
+  // Remove at index
+  removeAt(index) {
+    if (index > 0 && index > this.size) {
+      return;
+    }
+
+    let current = this.head;
+    let previous;
+    let count = 0;
+
+    // Remove first
+    if (index === 0) {
+      this.head = current.next;
+    } else {
+      while (count < index) {
+        count++;
+        previous = current;
+        current = current.next;
+      }
+
+      previous.next = current.next;
+    }
+
+    this.size--;
+  }
+  // Clear list
+  clearList() {
+    this.head = null;
+    this.size = 0;
+  }
+  // Print list data
+  printListData() {
+    let current = this.head;
+
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
+
+const ll = new LinkedList();
+
+ll.insertFirst(100);
+ll.insertFirst(200);
+ll.insertFirst(300);
+ll.insertLast(400);
+// ll.insertAt(500, 1);
+
+ll.printListData();
+// ll.removeAt(89);
+ll.clearList();
+console.log("clearing");
+ll.printListData();
+
+////////////////////////////////////////
+// ------------  July 24th   ------------
+// function findShort(s) {
+//   return s
+//     .split(" ")
+//     .map((el) => el.length)
+//     .sort((a, b) => a - b)[0];
+// }
+
+// console.log(
+//   findShort(
+//     "turns out random test cases are easier than writing out basic ones"
+//   ),
+//   3
+// );
 ////////////////////////////////////////
 // ------------  July 23rd   ------------
 // function findUniq(arr) {
